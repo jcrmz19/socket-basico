e0981871d1cf6de489a55a5f65143751af8489fc
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-03-04 13:23:07 -0600

    Video 176  + Retroalimentación de emisores del cliente hacia el servidor.

--- server/server.js
+++ server/server.js
@@ -29,8 +29,20 @@ io.on('connection', (client) => {
     });
 
     // Escuchar el cliente
-    client.on('enviarMensaje', (mensaje) => {
+    client.on('enviarMensaje', (mensaje, callback) => {
         console.log(mensaje);
+
+        if ( mensaje.usuario ) {
+            callback({
+                resp: 'TODO SALIO BIEN!'
+            });
+        } else {
+            callback({
+                resp: 'TODO SALIO MAL!!!!'
+            });
+        }
+
+
     });
 
 });
