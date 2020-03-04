f993927ec700c028f5f2122452415cd1f9459f14
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-03-04 13:16:14 -0600

    Video 175  + Emitir desde el servidor - escuchar en el cliente

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ io.on('connection', (client) => {
 
     console.log('Usuario conectado');
 
+    //
+    client.emit('enviarMensaje', {
+        usuario: 'Administrador',
+        mensaje: 'Bienvenido a esta aplicación'
+    });
+
     client.on('disconnect', () => {
         console.log('Usuario desconectado');
     });
